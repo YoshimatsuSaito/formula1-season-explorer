a1ee74665e4a7f276b518d1f9ff9cd5fe29952bd
YoshimatsuSaito <middle158725071@gmail.com>
2024-03-31 00:44:10 +0000

    add prediction

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ from dotenv import load_dotenv
 from modules.ergast_api import RoundData, retrieve_data
 from modules.geo import load_geo_data
 from modules.model.current_data_retriever import load_current_data
+from modules.model.model import Classifier, Ranker
+from modules.model.preprocess import ModelInputData, make_model_input_data
 from modules.utils import (get_target_previous_season_round, get_target_round,
                            get_target_season, load_config, retrieve_basic_info)
 
@@ -58,15 +60,78 @@ def cached_load_geo_data(geo_file_name: str) -> dict:
     )
 
 
-@st.cache(ttl=60 * 60, show_spinner=False)
-def cached_load_current_data(season: int) -> pd.DataFrame:
-    return load_current_data(
+@st.cache_data(ttl=60 * 60, show_spinner=False)
+def cached_make_model_input_data(season: int) -> pd.DataFrame:
+    df = load_current_data(
         season,
         bucket_name=BUCKET_NAME,
         aws_access_key_id=AWS_ACCESS_KEY_ID,
         aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
         s3_current_data_key=DICT_CONFIG["s3_current_data_key"],
     )
+
+    model_input_data = make_model_input_data(df)
+    return model_input_data
+
+
+@st.cache_resource(ttl=60 * 60 * 24, show_spinner=False)
+def cached_ranker() -> Ranker:
+    ranker = Ranker(
+        bucket_name=BUCKET_NAME,
+        aws_access_key_id=AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+        s3_model_key=DICT_CONFIG["s3_model_key"],
+    )
+    ranker.load_model()
+    return ranker
+
+
+@st.cache_resource(ttl=60 * 60 * 24, show_spinner=False)
+def cached_classifier() -> Classifier:
+    classifier = Classifier(
+        bucket_name=BUCKET_NAME,
+        aws_access_key_id=AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+        s3_model_key=DICT_CONFIG["s3_model_key"],
+    )
+    classifier.load_model()
+    return classifier
+
+
+@st.cache_data(ttl=60 * 60, show_spinner=False)
+def cached_winner_prediction_result(
+    model_input_data: ModelInputData,
+    _classifier: Classifier,
+    season: int,
+    round_to_show: int,
+) -> pd.DataFrame:
+
+    df = model_input_data.df.copy()
+    df = df.loc[(df["year"] == season) & (df["round"] == round_to_show)]
+    X = df[model_input_data.list_col_X]
+    y_pred = _classifier.predict(X)
+    df["y_pred"] = y_pred
+    df.sort_values(by="y_pred", ascending=False, inplace=True)
+
+    return df
+
+
+@st.cache_data(ttl=60 * 60, show_spinner=False)
+def cached_rank_prediction_result(
+    model_input_data: ModelInputData,
+    _ranker: Ranker,
+    season: int,
+    round_to_show: int,
+) -> pd.DataFrame:
+
+    df = model_input_data.df.copy()
+    df = df.loc[(df["year"] == season) & (df["round"] == round_to_show)]
+    X = df[model_input_data.list_col_X]
+    y_pred = _ranker.predict(X)
+    df["y_pred"] = y_pred
+    df.sort_values(by="y_pred", ascending=True, inplace=True)
+
+    return df
 
 
 # Identify the target round to show
@@ -125,11 +190,7 @@ st.write(
 # Show the circuits layout
 st.subheader("Circuit Layout")
 geojson_data = cached_load_geo_data(
-    geo_file_name=DICT_CONFIG["gp_circuits"][round_data.gp_name],
-    aws_access_key_id=AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-    bucket_name=BUCKET_NAME,
-    s3_geo_data_key=DICT_CONFIG["s3_geo_data_key"],
+    geo_file_name=DICT_CONFIG["gp_circuits"][round_data.gp_name]
 )
 chart_data = pd.DataFrame(
     geojson_data["features"][0]["geometry"]["coordinates"], columns=["lon", "lat"]
@@ -144,3 +205,18 @@ if previous_data is not None:
     st.dataframe(previous_data.df_race_result)
 else:
     st.warning("There is not available data")
+
+# Show race prediction
+st.subheader("AI Prediction")
+model_input_data = cached_make_model_input_data(SEASON)
+ranker = cached_ranker()
+classifier = cached_classifier()
+df_winner_prediction_result = cached_winner_prediction_result(
+    model_input_data, classifier, SEASON, round_to_show
+)
+st.dataframe(df_winner_prediction_result)
+
+df_rank_prediction_result = cached_rank_prediction_result(
+    model_input_data, ranker, SEASON, round_to_show
+)
+st.dataframe(df_rank_prediction_result)
